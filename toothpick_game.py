fe33259eeaadfe9f2fccfb937bb46e1bd85e45c2
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-07 00:06:48 +0000

    Refactor game

--- toothpick_game.py
+++ toothpick_game.py
@@ -3,22 +3,22 @@ print("********************")
 num_left = 13
 player1_name = input("Enter player 1's name: ")
 player2_name = input("Enter player 2's name: ")
+current_player = player1_name
 
 while True:
     print("| " * num_left)
     print(f"There are {num_left} toothpicks left")
-    p1_choice = int(input(f"{player1_name}, how many toothpicks do you take? "))
-    num_left -= p1_choice
+    choice = int(input(f"{current_player}, how many toothpicks do you take? "))
+    while choice != 1 and choice != 2 and choice != 3:
+        choice = int(input(f"You can only choose 1, 2 or 3: "))
+    num_left -= choice
     if num_left <= 0:
-        print(f"{player1_name} wins the game!")
+        print(f"{current_player} wins the game!")
         break
+    if current_player == player1_name:
+        current_player = player2_name
+    else:
+        current_player = player1_name    
 
-    print("| " * num_left)
-    print(f"There are {num_left} toothpicks left")
-    p2_choice = int(input(f"{player2_name}, how many toothpicks do you take? "))
-    num_left -= p2_choice
-    if num_left <= 0:
-        print(f"{player2_name} wins the game!")
-        break
 
 print("GAME OVER!")
